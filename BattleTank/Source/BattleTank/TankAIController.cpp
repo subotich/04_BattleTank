aa22be0202c5ad37bf33d6138218087dc1f56058
subotich <suboti.ch@icloud.com>
2019-01-24 07:39:20 +0100

    Updated map; Added AI tank controller

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Not possesing AI tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AIController possesd %s"), *(ControlledTank->GetName()));
+	}
+
+}
