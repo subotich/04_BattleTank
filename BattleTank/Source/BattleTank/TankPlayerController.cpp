a2d7184a953771a4503ea2cc1ba9eb8e0270f729
subotich <suboti.ch@icloud.com>
2019-01-28 08:11:33 +0100

    Find the crosshair position in pixel coordinates

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -38,7 +38,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 
 	if (GetSightRayHitLocation(HitLocation))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation %s"), *(HitLocation.ToString()));
+		// UE_LOG(LogTemp, Warning, TEXT("Look direction %s"), *(HitLocation.ToString()));
 		// Tell controlled tank to aim at this point
 	}
 }
@@ -46,6 +46,15 @@ void ATankPlayerController::AimTowardsCrosshair()
 // Get world location if linetrace through crosshair, true if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
 {
-	HitLocation = FVector(1.0);
+	// Find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+	UE_LOG(LogTemp, Warning, TEXT("Look direction %s"), *(ScreenLocation.ToString()));
+
+	// De-project the screen position of the crosshair to a world direction
+	// Line-trace along that look direction and see what we hit (up to max range)
+
+
 	return true;
 }
