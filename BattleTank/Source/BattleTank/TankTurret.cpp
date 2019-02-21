5b8f221a70906ee0199a74670105ecff181a7a2b
subotich <suboti.ch@icloud.com>
2019-02-21 01:38:48 +0100

    Added UTankTurret::Rotate method; Implementes Turret->Rotate; Updates default values;

--- BattleTank/Source/BattleTank/TankTurret.cpp
+++ BattleTank/Source/BattleTank/TankTurret.cpp
@@ -2,3 +2,18 @@
 
 #include "TankTurret.h"
 
+void UTankTurret::Rotate(float RelativeSpeed)
+{
+	// Move the barrel the right amount this frame
+	// Given the max elevation speed and frame time
+
+	//auto Time = GetWorld()->GetTimeSeconds();
+	//UE_LOG(LogTemp, Warning, TEXT("%f: Barrel elevated at speed %f"), Time, RelativeSpeed);
+
+	auto ClampedRelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1.0f, +1.0f);
+
+	auto RotationChange = ClampedRelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto Rotation = RelativeRotation.Yaw + RotationChange;
+
+	SetRelativeRotation(FRotator(0, Rotation, 0));
+}
--- BattleTank/Source/BattleTank/TankTurret.h
+++ BattleTank/Source/BattleTank/TankTurret.h
@@ -14,4 +14,11 @@ class BATTLETANK_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
+public:
+	// -1 is downward speed and +1 is max up movement
+	void Rotate(float RelativeSpeed);
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreesPerSecond = 20.0;
+
 };
