28a210b997346a3cf463bb94fc8a823101bb7a94
subotich <suboti.ch@icloud.com>
2019-03-09 10:29:06 +0100

    Implements RequestDirectMove on TankMovementComponenet; Implements MoveToActor on ATankAIController

--- BattleTank/Source/BattleTank/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/TankMovementComponent.h
@@ -30,6 +30,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendTurnLeft(float Throw);
 
+	// TODO check best protection
+	virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override; 
+
 private:
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
