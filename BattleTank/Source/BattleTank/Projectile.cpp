948a2ed622c4c783a82d6adc281e1e65c1b5609b
subotich <suboti.ch@icloud.com>
2019-03-30 17:02:31 +0100

    Implements explosion force

--- BattleTank/Source/BattleTank/Projectile.cpp
+++ BattleTank/Source/BattleTank/Projectile.cpp
@@ -23,6 +23,9 @@ AProjectile::AProjectile()
 	ImpactBlast->SetupAttachment(CollisionMesh);
 	ImpactBlast->bAutoActivate = false;
 
+	ExplosionForce = CreateDefaultSubobject<URadialForceComponent>(FName("Explosion Force"));
+	ExplosionForce->SetupAttachment(CollisionMesh);
+
 	// No need to protect pointers as added at construction
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("ProjectileMovement Component"));
 	ProjectileMovement->bAutoActivate = false;
@@ -40,6 +43,7 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, U
 	//UE_LOG(LogTemp, Warning, TEXT("LaunchProjectile onHit"));
 	LaunchBlast->Deactivate();
 	ImpactBlast->Activate();
+	ExplosionForce->FireImpulse();
 }
 
 
--- BattleTank/Source/BattleTank/Projectile.h
+++ BattleTank/Source/BattleTank/Projectile.h
@@ -10,6 +10,7 @@
 class UProjectileMovementComponent;
 class UStaticMeshComponent;
 class UParticleSystemComponent;
+class URadialForceComponent;
 
 UCLASS()
 class BATTLETANK_API AProjectile : public AActor
@@ -41,4 +42,7 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UParticleSystemComponent* ImpactBlast = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	URadialForceComponent* ExplosionForce = nullptr;
 };
