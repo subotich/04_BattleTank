e741b03249a3638878e063af71dac3dc9e9c0174
subotich <suboti.ch@icloud.com>
2019-04-05 23:32:18 +0200

    Added SpawnPoint

---
+++ BattleTank/Source/BattleTank/Public/SpawnPoint.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "SpawnPoint.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API USpawnPoint : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USpawnPoint();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	TSubclassOf<AActor> SpwanClass;
+
+};
